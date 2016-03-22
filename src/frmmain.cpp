e426691f02e669ed557fac2f8fb2cbc2cf499253
Denvi <denis_vic@mail.ru>
2016-03-22 19:40:49 +0500

    Revert port init proc (iss44)

--- src/frmmain.cpp
+++ src/frmmain.cpp
@@ -134,21 +134,18 @@ frmMain::frmMain(QWidget *parent) :
 //    ui->tblProgram->showColumn(4);
 
     // Setup serial port
-    connect(&m_serialPort, SIGNAL(error(QSerialPort::SerialPortError)), this, SLOT(onSerialPortError(QSerialPort::SerialPortError)));
-
-    if (m_settings.port() != "") {
-        m_serialPort.setPortName(m_settings.port());
-    }
-
-    openPort();
-
-    m_serialPort.setBaudRate(m_settings.baud());
     m_serialPort.setParity(QSerialPort::NoParity);
     m_serialPort.setDataBits(QSerialPort::Data8);
     m_serialPort.setFlowControl(QSerialPort::NoFlowControl);
     m_serialPort.setStopBits(QSerialPort::OneStop);
 
+    if (m_settings.port() != "") {
+        m_serialPort.setPortName(m_settings.port());
+        m_serialPort.setBaudRate(m_settings.baud());
+    }
+
     connect(&m_serialPort, SIGNAL(readyRead()), this, SLOT(onSerialPortReadyRead()));
+    connect(&m_serialPort, SIGNAL(error(QSerialPort::SerialPortError)), this, SLOT(onSerialPortError(QSerialPort::SerialPortError)));
 
     // Apply settings    
     foreach (StyledToolButton* button, ui->grpJog->findChildren<StyledToolButton*>(QRegExp("cmdJogStep\\d")))
