416237daee55d7e2c8f55b405474ef17e4a81480
Denvi <denis_vic@mail.ru>
2016-12-25 15:48:04 +0500

    Auto tool positioning on "Send from line" command

--- src/frmmain.cpp
+++ src/frmmain.cpp
@@ -1070,14 +1070,17 @@ void frmMain::onSerialPortReadyRead()
                     if (state.contains("S") || state.contains("C")) {
                         m_timerToolAnimation.start(25, this);
                         ui->cmdSpindle->setChecked(true);
+                    } else {
+                        m_timerToolAnimation.stop();
+                        ui->cmdSpindle->setChecked(false);
                     }
+
                     if (!pinState.isEmpty()) pinState.append(" / ");
                     pinState.append(QString(tr("AS: %1")).arg(as.cap(1)));
                 } else {
                     m_timerToolAnimation.stop();
                     ui->cmdSpindle->setChecked(false);
                 }
-
                 ui->glwVisualizer->setPinState(pinState);
             }
 
@@ -1382,25 +1385,17 @@ void frmMain::onSerialPortError(QSerialPort::SerialPortError error)
 
 void frmMain::onTimerConnection()
 {
-    qDebug() << "DTR" << m_serialPort.isDataTerminalReady();
-
     if (!m_serialPort.isOpen()) {
         openPort();
     } else if (!m_homing/* && !m_reseting*/ && !ui->cmdFilePause->isChecked() && m_queue.length() == 0) {
         if (m_updateSpindleSpeed) {
             m_updateSpindleSpeed = false;
-//            sendCommand(QString("S%1").arg(ui->txtSpindleSpeed->value()), -2, m_settings->showUICommands());
             sendCommand(QString("S%1").arg(ui->slbSpindle->value()), -2, m_settings->showUICommands());
         }
         if (m_updateParserStatus) {
             m_updateParserStatus = false;
             sendCommand("$G", -3, false);
         }
-//        if (m_updateFeed) {
-//            m_updateFeed = false;
-//            sendCommand(QString("F%1").arg(ui->chkFeedOverride->isChecked() ?
-//                m_originalFeed / 100 * ui->txtFeed->value() : m_originalFeed), -1, m_settings->showUICommands());
-//        }
     }
 }
 
@@ -1891,24 +1886,33 @@ void frmMain::onActSendFromLineTriggered()
         QVector<QList<int>> lineIndexes = parser->getLinesIndexes();
 
         int lineNumber = m_currentModel->data(m_currentModel->index(commandIndex, 4)).toInt();
-        LineSegment* segment = list.at(lineIndexes.at(lineNumber).last());
-        LineSegment* feedSegment = segment;
+        LineSegment* firstSegment = list.at(lineIndexes.at(lineNumber).first());
+        LineSegment* lastSegment = list.at(lineIndexes.at(lineNumber).last());
+        LineSegment* feedSegment = lastSegment;
         int segmentIndex = list.indexOf(feedSegment);
         while (feedSegment->isFastTraverse() && segmentIndex > 0) feedSegment = list.at(--segmentIndex);
 
         QStringList commands;
-        commands.append(QString("%1F%2")
-                        .arg(segment->isMetric() ? "G21" : "G20")
-                        .arg(segment->isMetric() ? feedSegment->getSpeed() : feedSegment->getSpeed() / 25.4));
 
-        if (segment->isArc()) {
-            commands.append(segment->plane() == PointSegment::XY ? "G17"
-            : segment->plane() == PointSegment::ZX ? "G18" : "G19");
+        commands.append(QString("M3 S%1").arg(qMax<double>(lastSegment->getSpindleSpeed(), ui->slbSpindle->value())));
+
+        commands.append(QString("G21 G90 G0 X%1 Y%2")
+                        .arg(firstSegment->getStart().x())
+                        .arg(firstSegment->getStart().y()));
+        commands.append(QString("G1 Z%1 F%2")
+                        .arg(firstSegment->getStart().z())
+                        .arg(feedSegment->getSpeed()));
+
+        commands.append(QString("%1 %2 %3 F%4")
+                        .arg(lastSegment->isMetric() ? "G21" : "G20")
+                        .arg(lastSegment->isAbsolute() ? "G90" : "G91")
+                        .arg(lastSegment->isFastTraverse() ? "G0" : "G1")
+                        .arg(lastSegment->isMetric() ? feedSegment->getSpeed() : feedSegment->getSpeed() / 25.4));
+
+        if (lastSegment->isArc()) {
+            commands.append(lastSegment->plane() == PointSegment::XY ? "G17"
+            : lastSegment->plane() == PointSegment::ZX ? "G18" : "G19");
         }
-        commands.append(QString("%1%2")
-                       .arg(segment->isFastTraverse() ? "G0" : "G1")
-                       .arg(segment->isAbsolute() ? "G90" : "G91"));
-        commands.append(QString("M3S%1").arg(qMax<double>(segment->getSpindleSpeed(), ui->slbSpindle->value())));
 
         QMessageBox box(this);
         box.setIcon(QMessageBox::Information);
