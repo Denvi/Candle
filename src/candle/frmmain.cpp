bd777df28a5332c5d1c2fd647e1ec782299e0ed5
Denvi <denis_vic@mail.ru>
2025-08-25 00:52:02 +0500

    Spindle speed slider step fixed

--- src/candle/frmmain.cpp
+++ src/candle/frmmain.cpp
@@ -2990,7 +2990,7 @@ void frmMain::applySettings() {
     ui->glwVisualizer->setColorBackground(m_settings->colors("VisualizerBackground"));
     ui->glwVisualizer->setColorText(m_settings->colors("VisualizerText"));
 
-    ui->slbSpindle->setRatio((m_settings->spindleSpeedMax() - m_settings->spindleSpeedMin()) / 100);
+    ui->slbSpindle->setRatio(m_settings->spindleSpeedMax() / 100);
     ui->slbSpindle->setMinimum(m_settings->spindleSpeedMin());
     ui->slbSpindle->setMaximum(m_settings->spindleSpeedMax());
 
