2ba939ab89590164feafb57494b83982ecc8a0a1
Denvi <denis_vic@mail.ru>
2025-11-12 08:40:25 +0500

    Extend VideoSurface supported formats, add vertical mirroring for win

--- src/designerplugins/cameraplugin/videosurface.cpp
+++ src/designerplugins/cameraplugin/videosurface.cpp
@@ -7,10 +7,29 @@ QList<QVideoFrame::PixelFormat> VideoSurface::supportedPixelFormats(QAbstractVid
 
     if (handleType == QAbstractVideoBuffer::NoHandle)
     {
-        formats << QVideoFrame::Format_RGB32
-                << QVideoFrame::Format_ARGB32
-                << QVideoFrame::Format_RGB24
-                << QVideoFrame::Format_BGR24;
+        formats 
+            << QVideoFrame::Format_ARGB32
+            << QVideoFrame::Format_ARGB32_Premultiplied
+            << QVideoFrame::Format_RGB32
+            << QVideoFrame::Format_RGB24
+            << QVideoFrame::Format_RGB565
+            << QVideoFrame::Format_RGB555
+            << QVideoFrame::Format_ARGB8565_Premultiplied
+            << QVideoFrame::Format_BGRA32
+            << QVideoFrame::Format_BGRA32_Premultiplied
+            << QVideoFrame::Format_BGR32
+            << QVideoFrame::Format_BGR24
+            << QVideoFrame::Format_BGR565
+            << QVideoFrame::Format_BGR555
+            << QVideoFrame::Format_AYUV444
+            << QVideoFrame::Format_YUV444
+            << QVideoFrame::Format_YUV420P
+            << QVideoFrame::Format_YV12
+            << QVideoFrame::Format_UYVY
+            << QVideoFrame::Format_YUYV
+            << QVideoFrame::Format_NV12
+            << QVideoFrame::Format_NV21
+            << QVideoFrame::Format_Jpeg;
     }
 
     return formats;
@@ -21,7 +40,11 @@ bool VideoSurface::present(const QVideoFrame &frame)
     if (!frame.isValid())
         return false;
 
+#ifdef Q_OS_WIN
+    emit frameReady(frame.image().mirrored(false, true));
+#else
     emit frameReady(frame.image());
+#endif
 
     return true;
 }
